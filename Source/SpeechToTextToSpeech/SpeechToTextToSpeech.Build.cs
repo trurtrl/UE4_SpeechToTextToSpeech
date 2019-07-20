7064436afb98b487723b9384f19aa36fec13dfd1
trurtrl <trurtrl@gmail.com>
2019-07-20 15:53:13 +0300

    Add empty plugin

--- Source/SpeechToTextToSpeech/SpeechToTextToSpeech.Build.cs
+++ Source/SpeechToTextToSpeech/SpeechToTextToSpeech.Build.cs
@@ -8,7 +8,7 @@ public class SpeechToTextToSpeech : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "StoTtoS" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
